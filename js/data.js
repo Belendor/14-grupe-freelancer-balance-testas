2eeeb66f81df23e8170f157891f40eed9b3cb8d2
Belendor <dwarf11@gmail.com>
2020-04-27 11:24:24 +0300

    eilutes

--- js/data.js
+++ js/data.js
@@ -1,3 +1,7 @@
+const contentBox = document.querySelector(".table-content")
+
+
+
 const account = [
     { month: 10, income: 150 },
     { month: 1, income: 200, expense: 50 },
@@ -12,3 +16,58 @@ const account = [
     { month: 8, income: 450, expense: 120 },
     { month: 12, expense: 200 }
 ];
+
+const menesiai = ["Sausis", "Vasaris", "Kovas", "Balandis", "Gegužė", "Birželis", "Liepa", "Rugpjūtis", "Rugsėjis", "Spalis", "Lapkritis", 'Gruodis']
+
+// for(let i = 0; i< account.length;i++){
+
+//     let objektass  = account.I
+
+
+// for (var prop in objektass) {
+//     if(objektass[prop] == "month"){
+//         objektass[prop]  = menesiai[i]
+
+//         console.log(prop)
+//     }
+//     break;
+// }
+// }
+
+
+
+let HTML = ""
+
+function pusinsRows ( menesioNumeris, income, islaidos){
+   
+    let balansas = 0
+    if(income === undefined || islaidos === undefined ){
+        balansas = income - islaidos
+    }
+        let HTML = 
+        `   <div class="table-row">
+        <div class="cell">${menesioNumeris}</div>
+        <div class="cell">Sausis</div>
+        <div class="cell">${income} Eur</div>
+        <div class="cell">-</div>
+        <div class="cell">${income} Eur</div>
+        </div>`
+
+    contentBox.insertAdjacentHTML("beforeend", HTML )
+}
+
+pusinsRows ( 10, 150, undefined)
+pusinsRows ( 1, 200, 50)
+pusinsRows ( 3, 450, 200)
+pusinsRows ( 7, 50, undefined)
+pusinsRows ( 2, 50, undefined)
+pusinsRows ( 2, 50, undefined)
+pusinsRows ( 5, 150, 50)
+pusinsRows ( 9, 700, 400)
+pusinsRows ( 4, 10, 300)
+pusinsRows ( 6, 50, undefined)
+pusinsRows ( 11, 150,30)
+pusinsRows ( 8, 450, 120)
+pusinsRows ( 12, 200, undefined)
+
+
